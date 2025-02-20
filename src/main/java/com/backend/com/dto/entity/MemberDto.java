19a88435876ab3b5b50d662afa2512c04aa801b0
kimyounchul <younchul@netro.kr>
2025-02-20 18:02:42 +0900

    zxczxczxc

---
+++ src/main/java/com/backend/com/dto/entity/MemberDto.java
@@ -0,0 +1,7 @@
+//package com.backend.com.dto.entity;
+//
+//public class MemberDto {
+//private String memberId;
+//private String accessToken;
+//private String recf
+//}
---
+++ src/main/java/com/backend/com/utils/SecurityUtil.java
@@ -0,0 +1,22 @@
+package com.backend.com.utils;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class SecurityUtil {
+	public static String getCurrentUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return null;
+        }
+
+        Object principal = authentication.getPrincipal();
+        if (principal instanceof UserDetails) {
+            return ((UserDetails) principal).getUsername();
+        } else {
+            return principal.toString();  // 일반적으로 username이 들어있음
+        }
+    }
+}
